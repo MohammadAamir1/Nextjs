4b20296f8018488c173779975c6c550153270b06
mohdamircode <amir113918amir@gmail.com>
2026-08-06 23:55:05 -0700

    writing backend code

---
+++ 06_writing-backend-code-in-nextjs/app/todos/route.js
@@ -0,0 +1,24 @@
+// import { writeFile } from "fs/promises";
+import { readFile } from "fs/promises";
+
+// await writeFile("hello.txt", 'Hi, How are you?')
+const fileContents = await readFile("hello.txt", "utf-8");
+console.log(fileContents);
+
+// console.log(process.cwd());
+
+// console.log("Written to the file");
+
+// this page also run in server page as page.js of main file
+
+
+import http from 'http'
+
+const server = http.createServer((req,res) => {
+  console.log(req.url);
+  res.end("Hello from new next.js server");
+});
+
+server.listen(4000, () => {
+  console.log('server started on port 4000');
+})
